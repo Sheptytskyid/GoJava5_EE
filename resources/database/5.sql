f54e1c4f086a09c48098daa46f7e008d30b3963e
Denys Sheptytskyi <neoxesis@mail.ru>
2017-02-17 20:52:16 +0200

    added another solution to 5.sql without temp table

--- resources/database/5.sql
+++ resources/database/5.sql
@@ -1,27 +1,46 @@
-#this solution works correctly
-CREATE TEMPORARY TABLE max_companies_projects
-  SELECT
-    companies.name     AS company,
-    min(projects.cost) AS revenue
-  FROM projects, companies
-  WHERE projects.company_id = companies.id
-  GROUP BY company_id;
-
 SELECT
-  a.company,
-  a.revenue,
-  c.name
-FROM max_companies_projects a
-  JOIN (projects b, customers c) ON (a.revenue = b.cost AND b.customer_id = c.id);
+  c.company,
+  a.name as customer,
+  c.revenue
+FROM customers a
+  JOIN projects b
+  JOIN (
+         SELECT
+           companies.name     AS company,
+           min(projects.cost) AS revenue
+         FROM projects, companies
+         WHERE projects.company_id = companies.id
+         GROUP BY company_id) c
+    ON (c.revenue = b.cost AND b.customer_id = a.id);
 
-DROP TABLE max_companies_projects;
+#solution with temporary table
+
+#CREATE TEMPORARY TABLE max_companies_projects
+#SELECT
+#companies.name     AS company,
+#min(projects.cost) AS revenue
+#FROM projects, companies
+#WHERE projects.company_id = companies.id
+#GROUP BY company_id;
+
+#SELECT
+#a.company,
+#a.revenue,
+#c.name
+#FROM max_companies_projects a
+#JOIN (projects b, customers c) ON (a.revenue = b.cost AND b.customer_id = c.id);
+
+#DROP TABLE max_companies_projects;
+
 
 #this solution is intended to do the same work as the upper one but it incorrectly matches customers to projects
 #  and I haven't yet found a way to fix it
-SELECT
-  companies.name     AS company,
-  customers.name     AS customer,
-  min(projects.cost) AS revenue
-FROM projects, companies, customers
-WHERE projects.company_id = companies.id AND projects.customer_id = customers.id
-GROUP BY company_id;
+
+#SELECT
+#companies.name     AS company,
+#customers.name     AS customer,
+#min(projects.cost) AS revenue
+#FROM projects, companies, customers
+#WHERE projects.company_id = companies.id AND projects.customer_id = customers.id
+#GROUP BY company_id;
+
